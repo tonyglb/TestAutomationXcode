99de2288db69e6887974a85e5cc61632bd4c0dbe
Antonio Aguilar <antonio.aguilar@globant.com>
2018-03-26 19:05:01 -0600

    init

---
+++ taxcode/taxcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taxcode
+//
+//  Created by tony on 26/03/18.
+//  Copyright © 2018 globant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
